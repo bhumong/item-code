65d0d980605b3b1e9909d8ea0ceae7dd5673e69f
bhumong <samuel.bhumong@gmail.com>
2026-08-09 14:19:05 +0700

    chore: scaffold ocr search backend module and docker compose

---
+++ backend/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/plugins/migratecmd"
+	"github.com/pocketbase/pocketbase/tools/osutils"
+)
+
+func main() {
+	app := pocketbase.New()
+
+	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
+		Automigrate: osutils.IsProbablyGoRun(),
+	})
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
